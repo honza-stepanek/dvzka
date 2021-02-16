bc7d4bc57b6fa69cc6b9925565bbe8bb18471ba1
honzaflash <jrychlyg@gmail.com>
2021-02-16 16:13:04 +0100

    upravena a opravena animace zvetsovani obrazku

--- dvz-mandelinka/expand-images.js
+++ dvz-mandelinka/expand-images.js
@@ -1,39 +1,58 @@
-
-$('.modal-image-container').click(function () {
+/**
+ * script setting up event listeners for clicks on modal images
+ * requires anim.js and jQuery
+ */
+$('.modal-image-container').click(function (event) {
     if ($(this).toggleClass('modal-expanded').hasClass('modal-expanded')) {
-        let maxWidth = parseInt($('tw-story').css('width'));
-        let maxHeight = parseInt($('tw-story').css('height'));
+        // expanding
 
-        let curW = $(this).width();
+        $(this).css('transition', '');
+        // clearing transition from potential previous run of the else branch 
 
-        let imgW = $('.modal-image-container.modal-expanded img').css('width');
-        let pHeight = $('.modal-image-container.modal-expanded p').css('width', imgW).height();
-        let curH = $(this).height() + pHeight;
-
-        console.log(maxWidth, curW, maxHeight, curH);
-
-        let scl = Math.min(maxWidth / (curW + 30), maxHeight / (curH + 50), 2);
+        const maxWidth = parseInt($('tw-story').css('width'));
+        const curW = $(this).width();
         
-        let transX = $('tw-passage').width() / 2 - curW / 2;
-        let transY = maxHeight / 2 - (($(this).offset().top + $('tw-passage').offset().top) + curH / 2);
-        // TODO make better Y translate descision based on height of the image and current position of viewport, etc.
-        console.log('translating x:', transX, 'translating y:', transY, 'scaling:', scl);
+        const scl = Math.min(maxWidth / (curW + 20), 2);
+        const transX = $('tw-passage').width() / 2 - curW / 2;
+        
+        const pHeight = $(this).children('p').css({'width': curW, 'height': 'auto'}).height();
+        const divHeight = $(this).height();
+        const topOverflow = ($(this)[0].getBoundingClientRect().top + 
+                          window.scrollY -
+                          divHeight * (scl - 1) / 2 -
+                          20) * -1;
+        const transY = Math.max(0, topOverflow);
+        const bottomOverflow = ($('tw-passage')[0].getBoundingClientRect().bottom -
+                              $(this)[0].getBoundingClientRect().bottom - 
+                              divHeight * (scl - 1) / 2 -
+                              pHeight * scl) * -1 +
+                              transY + 40;
+
+        console.log('p height:', pHeight * scl, 'upper overflow:', topOverflow, 'bottom overflow:', bottomOverflow, 'scaling:', scl);
         
         $(this).prop('modal-animation', anime({
-            targets: document.querySelectorAll('.modal-expanded'),
+            targets: $(this)[0],
             translateX: transX,
             translateY: transY,
             scale: scl,
             duration: 400,
             easing: 'cubicBezier(0.660, 0.010, 0.485, 0.995)'
         }));
+
+        if (bottomOverflow > 0) {
+            $('tw-passage').css('padding-bottom', "+=" + bottomOverflow);
+        }
         $(this).css('z-index', 20);
         $(this).prop('modal-animation').play();
     } else {
+        // collapsing
+
         let modAnim = $(this).prop('modal-animation');
         modAnim.direction = "reverse";
         modAnim.complete = () => {
-            $(this)[0].style = "";
+            $(this)[0].style = "transition: transform 300ms";
+            // if the div hasn't come back to its original place
+            // it will smoothly-ish transition back to it
         };
         modAnim.play();
     }
