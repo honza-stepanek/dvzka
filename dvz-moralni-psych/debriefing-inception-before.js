230629d0c6fb5dd584bcb81189fcaf3d9c4fa814
honzaflash <jrychlyg@gmail.com>
2021-03-16 21:43:49 +0100

    skripty pro inception-prechod do debriefingu

---
+++ dvz-moralni-psych/debriefing-inception-before.js
@@ -0,0 +1,18 @@
+// If needed to be still Twine compatible add:
+// $("head").append('<script src="https://cdn.jsdelivr.net/npm/html2canvas@1.0.0-rc.5/dist/html2canvas.min.js"></script>');
+{
+    $("tw-link").css("display", "none")[0].id = "actual-link";
+
+    $("<tw-link>Přejít na debriefing.</tw-link>", {
+        id: 'deroute-link'
+    }).on("click", async function() {
+        let canvas = await html2canvas(document.body, {height: window.innerHeight, y: window.scrollY});
+        canvas.style.position = "fixed";
+        canvas.style.top = "0";
+        canvas.style["z-index"] = "10";
+        await document.body.appendChild(canvas);
+        document.inceptionImage = await canvas.toDataURL();
+        
+        $("#actual-link").click();
+    }).appendTo("tw-passage");
+}
